ba65e9ba0ffbd01ea0bab756e9c65e717a44ea6c
MartinHajducik <41745906+MartinHajducik@users.noreply.github.com>
2024-09-20 12:36:33 +0200

    Update installAdobeAcrobatDC.ps1

--- installAdobeAcrobatDC.ps1
+++ installAdobeAcrobatDC.ps1
@@ -10,11 +10,15 @@ $webClient.DownloadFile($downloadUrl, $installerPath)
 Write-Host "Installing Adobe Reader silently..."
 Start-Process -FilePath $installerPath -ArgumentList "/sAll", "/rs", "/msi", "EULA_ACCEPT=YES" -Wait
 
-# Step 3: Set Adobe Reader as the default app for PDF using ADelRCP.exe
+# Step 3: Set Adobe Reader as the default app for PDF
 Write-Host "Setting Adobe Reader as the default app for PDFs..."
-$adelrcpPath = "$ProgramFiles\Adobe\Acrobat DC\Acrobat\ADelRCP.exe"
+$extension = ".pdf"
+$progId = "Acrobat.Document.DC"
 
-# Run ADelRCP.exe in elevated mode to set Adobe Reader as the default PDF handler
-Start-Process -FilePath $adelrcpPath -Verb RunAs
+# Use the "ftype" command to associate the file extension with the ProgID
+& cmd.exe /c "ftype $progId=`"%ProgramFiles%\Adobe\Acrobat DC\Acrobat\Acrobat.exe`" `%1"
+
+# Use "assoc" to associate .pdf with the program
+& cmd.exe /c "assoc $extension=$progId"
 
 Write-Host "Adobe Reader has been installed and set as the default PDF viewer."
